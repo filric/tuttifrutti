4e3cfbbcc3ab26fa70edbfab91d009b286513e77
filric <54800700+filric@users.noreply.github.com>
2020-08-31 14:08:24 +0200

    Weeeeeee

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,6 +7,29 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Niklas!");
+
+            
+
+
+            Console.WriteLine("Write your age:");
+
+            int myAge = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Write your name:");
+
+            string myName = Console.ReadLine();
+
+            Console.WriteLine("Your age is " + myAge);
+
+            Console.WriteLine("Your name is " + myName);
+
+            Console.WriteLine("Are you alive? ");
+
+            bool life = true;
+
+            Console.WriteLine(life);
+
+
         }
     }
 }
