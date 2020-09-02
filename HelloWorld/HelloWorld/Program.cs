857225d643e2a6385e93b0d062820ef26aab66c7
filric <54800700+filric@users.noreply.github.com>
2020-09-02 11:45:47 +0200

    Klar

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace HelloWorld
 {
@@ -6,12 +7,29 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Niklas!");
-
-            
 
 
-            Console.WriteLine("Write your age:");
+            Console.WriteLine("Choose a language you want to be greeted in.");
+            Console.WriteLine("Type 1 for english, 2 for japanese and 3 for klingon:");
+
+            var languageChoice = Convert.ToInt32(Console.ReadLine());
+
+            switch (languageChoice)
+            {
+                case 1:
+                    Console.WriteLine("Hello Niklas!");
+                    break;
+                case 2:
+                    Console.WriteLine("やあニクラス！");
+                    break;
+                case 3:
+                    Console.WriteLine("tlhIHqu' 'ej *tlhaw'DIyo* quv!");
+                    break;
+            }
+
+
+
+                    Console.WriteLine("Write your age:");
 
             int myAge = Convert.ToInt32(Console.ReadLine());
 
@@ -20,6 +38,28 @@ namespace HelloWorld
             string myName = Console.ReadLine();
 
             Console.WriteLine("Your age is " + myAge);
+            if (myAge > 17)
+            {
+                Console.WriteLine("Congratulations! You can now get a driverslicense!");
+            }
+            else
+            {
+                Console.WriteLine("You are a baby");
+            }
+            Console.WriteLine("Lets count from 0 to your age");
+            int i = 0;
+            while (i<= myAge)
+                {
+                Console.WriteLine(i);
+                i++;
+                }
+            Console.WriteLine("Lets count from your age to 0");
+            for (int j = myAge; j > -1; j--)
+            {
+                Console.WriteLine(j);
+            }
+
+
 
             Console.WriteLine("Your name is " + myName);
 
@@ -28,6 +68,7 @@ namespace HelloWorld
             bool life = true;
 
             Console.WriteLine(life);
+
 
 
         }
