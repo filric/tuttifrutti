647ca3324a9ea8b96b0c46825ff5ad2cf56325ad
filric <54800700+filric@users.noreply.github.com>
2020-09-09 11:11:45 +0200

    Klar

---
+++ HelloWorld/Projekt2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Projekt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            String[] names = new string[5];
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                names[i] = Console.ReadLine();
+                
+            }
+            Console.WriteLine("And here they are backwards: ");
+
+
+            for(int i = names.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+           
+        }
+    }
+}
