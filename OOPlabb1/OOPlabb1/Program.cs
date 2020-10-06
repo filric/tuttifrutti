581d49a930a6741aceff8a88fe33c4a8772c2062
filric <54800700+filric@users.noreply.github.com>
2020-10-06 10:19:33 +0200

    waa

--- OOPlabb1/OOPlabb1/Program.cs
+++ OOPlabb1/OOPlabb1/Program.cs
@@ -6,15 +6,38 @@ namespace OOPlabb1
     {
         static void Main(string[] args)
         {
+            int val;
+            do
+            {
+                Console.WriteLine("Du, mjölkhjärna, välj nåt av följande");
+                Console.WriteLine("1 = Lägg till kund och produkter");
+                Console.WriteLine("2 = Skriv ut produkter i kundkorg");
+                Console.WriteLine("9 = Avsluta och drick mjölk");
 
-            var kund = new Kund();
-            var produkt = new Produkt();
-            produkt.produktTyp = "småbarn";
-            kund.kundKorg.Add(produkt);
-            
-            foreach(var prod in kund.kundKorg){
-                Console.WriteLine(prod.produktTyp);
-            }
+                val = Convert.ToInt32(Console.ReadLine());
+
+                var kund = new Kund();
+                var produkt = new Produkt();
+
+                if (val == 1)
+                {
+                    produkt.produktTyp = "småbarn";
+                    kund.kundKorg.Add(produkt);
+                    Console.WriteLine("Produkt tillagd");
+                }
+                if (val == 2)
+                {
+                    for (int i = 0; i < kund.kundKorg.Count; i++)
+                    {
+                        Console.WriteLine(kund.kundKorg[i]);
+                    }
+                    //foreach (var prod in kund.kundKorg)
+                    //{
+                    //    Console.WriteLine(prod.produktTyp);
+                    //}
+                }
+            } while (val != 9);
+            Console.WriteLine("Programmet är avlsutat. Gå och drick mjölk");
         }
     }
 }
