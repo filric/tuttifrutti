61a90f90a9ee339228c4c65730630e3e26c352cf
filric <54800700+filric@users.noreply.github.com>
2020-11-02 14:55:52 +0100

    waa

---
+++ WPFProjekt/WPFProjekt/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFProjekt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button9_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonDecimal_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
